39e55b0b015847d071e491cad55d5807584a285b
Teck <ch0pst1x88@gmail.com>
2016-01-05 12:05:04 -0800

    login/reg, bcrypt, and DB features

--- server.js
+++ server.js
@@ -1,24 +1,47 @@
-// require the path module
 var path = require("path");
-// require express and create the express app
+
 var express = require("express");
 var app = express();
+
+var morgan = require("morgan");
 // require bodyParser since we need to handle post data for adding a user
 var bodyParser = require("body-parser");
-
 //User body parser to send JSON
-app.use(bodyParser.json());
+var passport = require("passport");
+var cookieParser = require("cookie-parser");
+var session = require ('express-session');
+
+var flash = require("connect-flash");
+
+require('./server/config/passport')(passport);
+
+app.use(morgan('dev'));
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(session({secret: "anystringoftext",
+				saveUninitialized: true,
+				resave: true}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+app.set('view engine', 'ejs')
 
 app.use(express.static(path.join(__dirname, "./client")));
 
 require('./server/config/mongoose.js');
-require('./server/config/routes.js')(app);
 
+require('./server/config/routes.js')(app, passport);
+
+// app.use('/', function(req, res){
+// 	console.log(req.cookies);
+// 	console.log("============");
+// 	console.log(req.session);
+// });
 
 // listen on 8000
 app.listen(8000, function() {
  console.log("listening on port 8000. dayPhrase is up and running!");
 })
-
-
-
